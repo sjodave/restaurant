a5c6c5eae34134ecd3d0f035e601a52eecc298fa
sjodave <sjodave@gmail.com>
2022-11-07 00:34:36 +0530

    c1

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -27,8 +27,8 @@ export default function Location({ restaurantName, isBookmark }) {
       </div>
       <iframe
         title="map"
-        width="500"
-        height="350"
+        width="600"
+        height="250"
         src={`https://datastudio.google.com/embed/reporting/430242fa-4162-4950-a984-824b3b355b3c/page/dQMwC?params={"ds2.name2":"${restaurantName}"}`}
         frameBorder="0"
         style={{ border: 0 }}
