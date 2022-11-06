a7f5e81e0b85d7129b904579b386e56b999c4416
sjodave <sjodave@gmail.com>
2022-11-06 23:07:38 +0530

    commit1

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -27,8 +27,8 @@ export default function Location({ restaurantName, isBookmark }) {
       </div>
       <iframe
         title="map"
-        width="600"
-        height="450"
+        width="500"
+        height="350"
         src={`https://datastudio.google.com/embed/reporting/430242fa-4162-4950-a984-824b3b355b3c/page/dQMwC?params={"ds2.name2":"${restaurantName}"}`}
         frameBorder="0"
         style={{ border: 0 }}
